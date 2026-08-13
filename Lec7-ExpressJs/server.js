08271f9e606e708c41ca94f8eee3ee0d419b9a5e
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-08-14 01:01:56 +0530

    Add Express JS setup

---
+++ Lec7-ExpressJs/server.js
@@ -0,0 +1,9 @@
+console.log("Starting with Express JS");
+
+console.log("Step 1: Install Express JS in the folder using npm i express");
+
+console.log("This will create the node_modules folder in the project, which stores Express and its dependencies");
+
+console.log("This will also create package-lock.json, which stores information about the exact versions and dependency tree");
+
+console.log("This will also add Express as a dependency in the package.json file");
