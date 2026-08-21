4e5ca63e936b38482661e6e01468e30e2e2f9cd0
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-08-21 23:52:29 +0530

    Learned how to connect Mongo DB with Express JS

---
+++ Lec14-MongoDB-ExpressJs/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+
+// since hum URL directly nhi rakhenge toh hum env file use krenge isliye ye import
+import dotenv from "dotenv";
+
+dotenv.config(); // .env file ke andar jo variables hain, unko Node.js ke environment variables mein load karega.
+
+console.log("Connecting MongoDB with ExpressJS");
+// Ye database se connect krne ke liye
+// MongoDB Atlas se connection string copy karke .env file mein store karenge
+// isko directly yaha pe bhi link daal skte hai but that is not recommended due to privacy
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { dbName: "NodeJs Mastery Course" }, // ye db name humko manually dena hota hai jo compass me dikhega
+  )
+  .then(() => console.log("MongoDB connected")) // ye jab databse se connect ho jaayega toh ye message display hoga
+  .catch((e) => console.log("Error : ", e)); // agar kuch error aaya toh ye error message display krega
+
+const app = express();
+
+const port = 3000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
